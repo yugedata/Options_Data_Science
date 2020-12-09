663941a4562d0bb5cb90c458a2d8df26f0570731
Drew Knapp <39703673+yugedata@users.noreply.github.com>
2020-12-09 18:39:11 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def history(symbol):
     return quotes
 
 
-
+'''
 test_quotes_2D = TDClient.get_quotes(TDSession, instruments=['AMD', 'AAPL'])
 
 
@@ -64,10 +64,10 @@ def stats_list():
                 output_stats.append(key)
 
     return output_stats
+'''
 
-
-test_quote_time_epoch = test_quotes_2D['AMD']['regularMarketTradeTimeInLong']
-human_time(test_quote_time_epoch)
+# test_quote_time_epoch = test_quotes_2D['AMD']['regularMarketTradeTimeInLong']
+# human_time(test_quote_time_epoch)
 
 
 opt_column_names = ['putCall', 'symbol', 'description', 'exchangeName', 'bid', 'ask', 'last', 'mark', 'bidSize',
@@ -94,11 +94,10 @@ stocks = ['AAL', 'AAPL', 'AMD', 'AMZN', 'APA', 'ATVI', 'AXP', 'BABA', 'BAC', 'BU
           'SNAP', 'SPY', 'SQ', 'TSLA', 'TWTR', 'ULTA', 'UPS', 'V', 'VXX', 'WMT', 'YUM',
           'VDE', 'XLB', 'XLI', 'VCR', 'VDC', 'XLV', 'XLF', 'VGT', 'XLC', 'XLU', 'VNQ']
 
-print(len(opt_column_names))
-print(len(columns_unwanted))
-print(len(columns_wanted))
-print(len(stocks))
-
+# print(len(opt_column_names))
+# print(len(columns_unwanted))
+# print(len(columns_wanted))
+# print(len(stocks))
 
 # /\ This segment was used to sort out unique columns after i hard coded the columns i wanted
 outs = []
@@ -117,31 +116,174 @@ def unique_list(n):
     return 0
 
 
+'''
 for i in opt_column_names:
     for j in columns_wanted:
         if i == j:
             outs.append(i)
-
-print(outs)
-print(len(outs))
-unique_list(outs)
+'''
+# print(outs)
+# print(len(outs))
+# unique_list(outs)
 
 # \/
 
 
-def get_chain(s):
+def get_chain(stock):
     opt_lookup = TDSession.get_options_chain(
-        option_chain={'symbol': s, 'strikeCount': 50,
+        option_chain={'symbol': stock, 'strikeCount': 50,
                       'toDate': '2021-2-28'})
 
     return opt_lookup
 
 
+'''
 def narrow_print():
     opt_lookup = get_chain('SPY')
     narrow = opt_lookup['putExpDateMap']['2020-11-27:1']['363.0'][0]
     for m in narrow.keys():
         print(m, narrow[m])
     return 0
+'''
 
 
+def raw_chain(raw, put_call):
+    cp = f'{put_call}ExpDateMap'
+    raw_data = [[]]
+    r = -1
+    for k in raw[cp].keys():
+        # print(k, raw[k], '\n')
+        for strike in raw[cp][k].keys():
+            # print(strike, raw[k][strike])
+            for a in raw[cp][k][strike][0].keys():
+                # if r == -1:
+                #    print(raw[cp][k][strike][0].keys())
+                unit = raw[cp][k][strike][0][a]
+                if unit == put_call.upper():
+                    r = r + 1
+                    if r > 0:
+                        raw_data.append([])
+
+                raw_data[r].append(unit)
+
+    return raw_data
+
+
+def clean_chain(raw):
+    df_cp = pd.DataFrame(raw, columns=opt_column_names)
+    clean = df_cp.drop(columns=columns_unwanted)
+
+    return clean
+
+
+def make_sqlite_table(table_name):
+    engine = create_engine('sqlite:///Options.db', echo=False)
+    table_columns = pd.DataFrame(columns=columns_wanted)
+    table_columns.to_sql(table_name, con=engine)
+
+    return 0
+
+
+#
+def add_rows(clean_data, table_name):
+    engine = create_engine('sqlite:///Options.db', echo=False)
+    clean_data.to_sql(table_name, con=engine, if_exists='append', index_label='index')
+
+    return 0
+
+
+def delete_db_table(table):
+    conn = sqlite3.connect('options.db')
+    con = conn.cursor()
+    con.execute(f'DROP TABLE {table}')
+    conn.commit()
+    conn.close()
+
+    return 0
+
+
+def show_db_table(puts_calls):
+    conn = sqlite3.connect('options.db')
+    con = conn.cursor()
+    for row in con.execute(f'SELECT * FROM {puts_calls}'):
+        print(row)
+    conn.close()
+
+    return 0
+
+
+pulls = 0
+failed_pulls = 0
+
+
+def get_next_chains():
+    x = 0
+    global pulls
+    global failed_pulls
+
+    for stock in stocks:
+        error = False
+        try:
+            chain = get_chain(stock)
+        except ServerError:
+            error = True
+            failed_pulls = failed_pulls + 1
+            print('A server error occurred')
+
+        if not error:
+            try:
+                add_rows(clean_chain(raw_chain(chain, 'call')), 'calls')
+                pulls = pulls + 1
+
+            except (ValueError, ServerError):
+                print(f'{x}: Calls for {stock} did not have values for this iteration')
+                failed_pulls = failed_pulls + 1
+
+            try:
+                add_rows(clean_chain(raw_chain(chain, 'put')), 'puts')
+                pulls = pulls + 1
+
+            except (ValueError, ServerError):
+                print(f'{x}: Puts for {stock} did not have values for this iteration')
+                failed_pulls = failed_pulls + 1
+
+        print(f'{x}: {stock}')
+        x = x + 1
+        time.sleep(2)
+
+    return 0
+
+
+def start():
+    pull_count = 0
+    end_t = 1600
+
+    while get_time_now() < end_t:
+        get_next_chains()
+        pull_count = pull_count + 1
+        print(pull_count)
+
+    print('option market closed')
+
+    print(f'failed_pulls: {failed_pulls}')
+    print(f'pulls: {pulls}')
+
+    return 0
+
+
+# |SQLite management| #
+#
+# make_sqlite_table('calls')  # inputs: puts|calls
+# add_rows(clean_chain(raw_chain(get_chain('SPY'), 'put')), 'puts')  # raw_chain(,'put|call')), 'puts|calls')
+# delete_db_table('calls')
+# delete_db_table('puts')
+# show_db_table('puts')
+# show_db_table('calls')
+
+
+def main():
+    start()
+    return 0
+
+
+main()
