0dc9be00d7b6d9b9a1787167e269a7f8d4dd0b0d
Drew Knapp <39703673+yugedata@users.noreply.github.com>
2020-12-08 23:59:42 -0500

    Add files via upload
    Create SQLite db and tables to store options data that is being collected from td ameritrade api every trading day. This file can also be used to query the db

---
+++ access.py
@@ -0,0 +1,83 @@
+from sqlalchemy import create_engine
+import _sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+conn = _sqlite3.connect('Options.db')
+c = conn.cursor()
+
+
+def test():
+    c.execute('SELECT * FROM puts WHERE rowid = 1')
+    one = c.fetchone()
+
+    for j in one:
+        print(j)
+
+    return 0
+
+
+# test()
+
+check, count = [], 0
+y_yes = []
+
+for row in c.execute(f'SELECT quoteTimeInLong FROM puts WHERE symbol = \'AAPL_121120P123\''):
+    for t in row:
+        temp = t // 100000
+        # print(temp)
+        check.append(temp)
+
+go = check[0]
+end = check[-1]
+
+x_time = np.arange(start=go, stop=end)
+
+for n in x_time:
+    if n == check[count]:
+        count = count + 1
+        y_yes.append(1)
+    else:
+        y_yes.append(.5)
+
+
+def equa_distanced():
+    n_bins = [go]
+    next_bin = go
+    diff = (end - go) // 20
+    count_2 = 0
+    while count_2 < 20:
+        n_bins.append(next_bin)
+        next_bin = next_bin + diff
+        print(next_bin)
+        print(diff)
+        count_2 = count_2 + 1
+
+    return 0
+
+
+columns_wanted = ['time']
+
+
+def make_sqlite_table(table_name):
+    engine = create_engine('sqlite:///temp.db', echo=False)
+    table_columns = pd.DataFrame(columns=columns_wanted)
+    table_columns.to_sql(table_name, con=engine)
+
+    return 0
+
+
+def add_rows(clean_data, table_name):
+    engine = create_engine('sqlite:///temp.db', echo=False)
+    clean_data.to_sql(table_name, con=engine, if_exists='append', index_label='index')
+
+    return 0
+
+
+# make_sqlite_table('Time')
+# add_rows(check, 'Time')
+plt.hist(x=check, bins=173, density=False)
+plt.title('Minutes Captured')
+plt.show()
+conn.close()
