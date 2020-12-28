7172e1dc77358af1979486f0d164fe63cab6cc1a
Drew Knapp <39703673+yugedata@users.noreply.github.com>
2020-12-28 16:43:04 -0500

    Update Trade.py

--- Trade.py
+++ Trade.py
@@ -8,27 +8,31 @@ import pandas as pd
 
 buying_power = 50000
 
-# columns: trade_id, openClose, time, contracts, quantities, prices, total, gainLoss, type, width
+cart = []
+
+columns = ['trade_id', 'openClose', 'time', 'contracts', 'quantities',
+           'prices', 'total', 'debitCredit', 'width']
 
 
 def make_sqlite_table(table_name):
 
     metadata = MetaData()
-    trades = Table('iron_condors', metadata,
-                   Column('trade_id', Integer, primary_key=True),
+    trades = Table(f'{table_name}', metadata,
+                   Column('trade_id', Integer),
                    Column('openClose', Integer),
                    Column('time', Integer),
                    Column('contracts', String),   # tuple
                    Column('quantities', String),  # tuple
                    Column('prices', String),      # tuple
                    Column('total', Integer),      # total of prices
-                   Column('gainLoss', Integer),
-                   Column('type', String),
-                   Column('width', String))
+                   Column('debitCredit', Integer),
+                   Column('width', Integer))
+
     engine = create_engine('sqlite:///Trades.db', echo=False)
     metadata.create_all(engine)
+
     inspector = inspect(engine)
-    inspector.get_columns('iron_condors')
+    inspector.get_columns(f'{table_name}')
 
     return 0
 
@@ -36,19 +40,7 @@ def make_sqlite_table(table_name):
 def add_rows(clean_data, table_name):
 
     engine = create_engine('sqlite:///Trades.db', echo=False)
-    with engine.connect() as con:
-
-        data = ({'trade_id': 1, 'openClose': 1, 'time': 1232456789,
-                 'contracts': '(AAPL_121120P123, AAPL_121120P123, AAPL_121120P123, AAPL_121120P123)',
-                 'quantities': '(1, 1, 1, 1)', 'prices': '(103, 50, 140, 60)',
-                 'total': 353, 'gainLoss': -100, 'width': 'tight'})
-
-        sql = text(f'''INSERT INTO {table_name}
-                (trade_id, openClose, time, contracts, quantities, prices, total, gainLoss, type, width)
-                VALUES(:time, :contracts, :quantities, :prices, :total, :gainLoss, :type, :width)''')
-
-        for line in data:
-            con.execute(sql, **line)
+    clean_data.to_sql(table_name, con=engine, if_exists='append', index_label='rowid')
 
     return 0
 
@@ -59,7 +51,6 @@ def show_table(table_name):
     with engine.connect() as con:
 
         rs = con.execute(f'SELECT * FROM {table_name}')
-
         for row in rs:
             print(row)
 
@@ -70,18 +61,28 @@ def delete_row(table_name, column, argument):
 
     engine = create_engine('sqlite:///Trades.db', echo=False)
     with engine.connect() as con:
+
         con.execute(f'DELETE FROM {table_name} WHERE {column}={argument}')
 
     return 0
+
+
+test_data = [[123, 1, 12321321321, f'(AAPL_121120P123, AAPL_121120P123, AAPL_121120P123, AAPL_121120P123)',
+              f'(1, 1, 1, 1)', f'(100, 50, 110, 60)', 90, -90, 8]]
+
+test_data = pd.DataFrame(test_data, columns=columns)
+
+make_sqlite_table('iron_condors')
+add_rows(test_data, 'iron_condors')
+show_table('iron_condors')
 
 
 # this func will act as a checkout cart, outputting the combination
 # of contracts needed to buy/sell to create the explicit option spread
 # output: tuple with only strings of contract(s) symbol/id
 def basket():
-    cart = []
-    return 0
 
+    global cart
 
-
+    return cart
 
