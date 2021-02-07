0737ad3f2cd6a74323659a6cd3f983bff41074ba
Drew Knapp <39703673+yugedata@users.noreply.github.com>
2021-02-07 08:30:42 -0500

    cleared testing code

--- mine.py
+++ mine.py
@@ -26,178 +26,6 @@ def history(symbol):
     return quotes
 
 
-'''
-test_quotes_2D = TDClient.get_quotes(TDSession, instruments=['AMD', 'AAPL'])
-
-
-def stats_list():
-    stats_wanted = ['symbol', 'bidPrice', 'bidSize', 'bidId', 'askPrice', 'askId',
-                    'lastPrice', 'lastSize', 'lastId', 'openPrice', 'highPrice',
-                    'lowPrice', 'bidTick', 'closePrice', 'netChange', 'totalVolume',
-                    'quoteTimeInLong', 'tradeTimeInLong', 'exchange',
-                    'exchangeName', 'volatility',
-                    'regularMarketLastPrice', 'regularMarketNetChange',
-                    'regularMarketTradeTimeInLong', 'netPercentChangeInDouble',
-                    'markChangeInDouble', 'markPercentChangeInDouble',
-                    'regularMarketPercentChangeInDouble']
-
-    output_stats = []
-
-    for key in test_quotes_2D['AMD'].keys():
-        for i in stats_wanted:
-            if key == i:
-                output_stats.append(key)
-
-    return output_stats
-'''
-
-file_date = 0
-
-trade_days_2021 = {'jan': [4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 19, 20, 21, 22, 25, 26, 27, 28, 29],
-                   'feb': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 16, 17, 18, 19, 22, 23, 24, 25, 26],
-                   'mar': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31],
-                   'apr': [5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30],
-                   'may': [3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28],
-                   'jun': [1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 28, 29, 30],
-                   'jul': [1, 2, 6, 7, 8, 9, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30],
-                   'aug': [2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 23, 24, 25, 26, 27, 30, 31],
-                   'sep': [1, 2, 3, 7, 8, 9, 10, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 27, 28, 29, 30],
-                   'oct': [1, 4, 5, 6, 7, 8, 12, 13, 14, 15, 18, 19, 20, 21, 22, 25, 26, 27, 28, 29],
-                   'nov': [1, 2, 3, 4, 5, 8, 9, 10, 12, 15, 16, 17, 18, 19, 22, 23, 24, 29, 30],
-                   'dec': [1, 2, 3, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 20, 21, 22, 27, 28, 29, 30]}
-
-
-opt_column_names = ['putCall', 'symbol', 'description', 'exchangeName', 'bid', 'ask', 'last', 'mark', 'bidSize',
-                    'askSize', 'bidAskSize', 'lastSize', 'highPrice', 'lowPrice', 'openPrice', 'closePrice',
-                    'totalVolume', 'tradeDate', 'tradeTimeInLong', 'quoteTimeInLong', 'netChange', 'volatility',
-                    'delta', 'gamma', 'theta', 'vega', 'rho', 'openInterest', 'timeValue', 'theoreticalOptionValue',
-                    'theoreticalVolatility', 'optionDeliverablesList', 'strikePrice', 'expirationDate',
-                    'daysToExpiration',
-                    'expirationType', 'lastTradingDay', 'multiplier', 'settlementType', 'deliverableNote',
-                    'isIndexOption', 'percentChange', 'markChange', 'markPercentChange', 'mini', 'inTheMoney',
-                    'nonStandard']
-
-columns_unwanted = ['description', 'mark', 'bidSize', 'askSize', 'bidAskSize', 'lastSize', 'tradeDate',
-                    'tradeTimeInLong', 'theoreticalOptionValue', 'optionDeliverablesList',
-                    'expirationType', 'lastTradingDay', 'multiplier', 'settlementType', 'deliverableNote',
-                    'isIndexOption', 'markChange', 'markPercentChange', 'nonStandard', 'inTheMoney', 'mini']
-
-columns_wanted = ['putCall', 'symbol', 'exchangeName', 'bid', 'ask', 'last', 'highPrice',
-                  'lowPrice', 'openPrice', 'closePrice', 'totalVolume', 'quoteTimeInLong',
-                  'netChange', 'volatility', 'delta', 'gamma', 'theta', 'vega', 'rho', 'openInterest', 'timeValue',
-                  'theoreticalVolatility', 'strikePrice', 'expirationDate', 'daysToExpiration', 'percentChange']
-
-stocks = ['AAL', 'AAPL', 'AMD', 'AMZN', 'APA', 'ATVI', 'AXP', 'BABA', 'BAC', 'C',
-          'CME', 'CMG', 'DAL', 'EA', 'FB', 'GOOG', 'GS', 'HD', 'IBM', 'JNJ', 'JPM',
-          'MCD', 'MSFT', 'MU', 'NEE', 'NFLX', 'NVDA', 'ORCL', 'PYPL', 'QQQ', 'ROKU',
-          'SNAP', 'SPY', 'SQ', 'TSLA', 'TWTR', 'ULTA', 'UPS', 'V', 'VXX',
-          'VDE', 'XLB', 'XLI', 'VCR', 'VDC', 'XLV', 'XLF', 'VGT', 'XLC', 'XLU', 'VNQ']
-
-# print(len(opt_column_names))
-# print(len(columns_unwanted))
-# print(len(columns_wanted))
-# print(len(stocks))
-
-# /\ This segment was used to sort out unique columns after i hard coded the columns i wanted
-outs = []
-
-
-def unique_list(n):
-    output = []
-
-    for x in n:
-        if x not in output:
-            output.append(x)
-        else:
-            print(x)
-    print(len(output))
-
-    return 0
-
-
-'''
-for i in opt_column_names:
-    for j in columns_wanted:
-        if i == j:
-            outs.append(i)
-'''
-
-
-# print(outs)
-# print(len(outs))
-# unique_list(outs)
-
-# \/
-
-
-def human_time(epoch):
-    new_time = datetime.fromtimestamp(int(epoch) / 1000)
-    output = new_time.strftime('%Y-%m-%d %H:%M:%S')
-
-    return output
-
-
-def get_time_now():
-    curr_time = time.localtime()
-    curr_clock = time.strftime("%H:%M:%S", curr_time)
-    curr_m = time.strftime('%m')
-    curr_y_d = time.strftime('%d%Y')
-
-    int_curr_clock = int(f'{curr_clock[:2]}{curr_clock[3:5]}')
-
-    return int_curr_clock, curr_m, curr_y_d
-
-
-def get_chain(stock):
-    opt_lookup = TDSession.get_options_chain(
-        option_chain={'symbol': stock, 'strikeCount': 50,
-                      'toDate': '2021-3-26'})
-
-    return opt_lookup
-
-
-''' Testing
-test_quote_time_epoch = test_quotes_2D['AMD']['regularMarketTradeTimeInLong']
-human_time(test_quote_time_epoch)
-
-def narrow_print():
-    opt_lookup = get_chain('SPY')
-    narrow = opt_lookup['putExpDateMap']['2020-11-27:1']['363.0'][0]
-    for m in narrow.keys():
-        print(m, narrow[m])
-    return 0
-'''
-
-
-def raw_chain(raw, put_call):
-    cp = f'{put_call}ExpDateMap'
-    raw_data = [[]]
-    r = -1
-    for k in raw[cp].keys():
-        # print(k, raw[k], '\n')
-        for strike in raw[cp][k].keys():
-            # print(strike, raw[k][strike])
-            for a in raw[cp][k][strike][0].keys():
-                # if r == -1:
-                #    print(raw[cp][k][strike][0].keys())
-                unit = raw[cp][k][strike][0][a]
-                if unit == put_call.upper():
-                    r = r + 1
-                    if r > 0:
-                        raw_data.append([])
-
-                raw_data[r].append(unit)
-
-    return raw_data
-
-
-def clean_chain(raw):
-    df_cp = pd.DataFrame(raw, columns=opt_column_names)
-    clean = df_cp.drop(columns=columns_unwanted)
-
-    return clean
-
-
 def make_sqlite_table(table_name):
     engine = create_engine('sqlite:///Options.db', echo=False)
     table_columns = pd.DataFrame(columns=columns_wanted)
@@ -242,6 +70,104 @@ def show_db_table(puts_calls):
     conn.close()
 
     return 0
+
+
+file_date = 0
+
+trade_days_2021 = {'jan': [4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 19, 20, 21, 22, 25, 26, 27, 28, 29],
+                   'feb': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 16, 17, 18, 19, 22, 23, 24, 25, 26],
+                   'mar': [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31],
+                   'apr': [5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30],
+                   'may': [3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28],
+                   'jun': [1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 28, 29, 30],
+                   'jul': [1, 2, 6, 7, 8, 9, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30],
+                   'aug': [2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 23, 24, 25, 26, 27, 30, 31],
+                   'sep': [1, 2, 3, 7, 8, 9, 10, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 27, 28, 29, 30],
+                   'oct': [1, 4, 5, 6, 7, 8, 12, 13, 14, 15, 18, 19, 20, 21, 22, 25, 26, 27, 28, 29],
+                   'nov': [1, 2, 3, 4, 5, 8, 9, 10, 12, 15, 16, 17, 18, 19, 22, 23, 24, 29, 30],
+                   'dec': [1, 2, 3, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 20, 21, 22, 27, 28, 29, 30]}
+
+
+opt_column_names = ['putCall', 'symbol', 'description', 'exchangeName', 'bid', 'ask', 'last', 'mark', 'bidSize',
+                    'askSize', 'bidAskSize', 'lastSize', 'highPrice', 'lowPrice', 'openPrice', 'closePrice',
+                    'totalVolume', 'tradeDate', 'tradeTimeInLong', 'quoteTimeInLong', 'netChange', 'volatility',
+                    'delta', 'gamma', 'theta', 'vega', 'rho', 'openInterest', 'timeValue', 'theoreticalOptionValue',
+                    'theoreticalVolatility', 'optionDeliverablesList', 'strikePrice', 'expirationDate',
+                    'daysToExpiration',
+                    'expirationType', 'lastTradingDay', 'multiplier', 'settlementType', 'deliverableNote',
+                    'isIndexOption', 'percentChange', 'markChange', 'markPercentChange', 'mini', 'inTheMoney',
+                    'nonStandard']
+
+columns_unwanted = ['description', 'mark', 'bidSize', 'askSize', 'bidAskSize', 'lastSize', 'tradeDate',
+                    'tradeTimeInLong', 'theoreticalOptionValue', 'optionDeliverablesList',
+                    'expirationType', 'lastTradingDay', 'multiplier', 'settlementType', 'deliverableNote',
+                    'isIndexOption', 'markChange', 'markPercentChange', 'nonStandard', 'inTheMoney', 'mini']
+
+columns_wanted = ['putCall', 'symbol', 'exchangeName', 'bid', 'ask', 'last', 'highPrice',
+                  'lowPrice', 'openPrice', 'closePrice', 'totalVolume', 'quoteTimeInLong',
+                  'netChange', 'volatility', 'delta', 'gamma', 'theta', 'vega', 'rho', 'openInterest', 'timeValue',
+                  'theoreticalVolatility', 'strikePrice', 'expirationDate', 'daysToExpiration', 'percentChange']
+
+stocks = ['AAL', 'AAPL', 'AMD', 'AMZN', 'APA', 'ATVI', 'AXP', 'BABA', 'BAC', 'C',
+          'CME', 'CMG', 'DAL', 'EA', 'FB', 'GOOG', 'GS', 'HD', 'IBM', 'JNJ', 'JPM',
+          'MCD', 'MSFT', 'MU', 'NEE', 'NFLX', 'NVDA', 'ORCL', 'PYPL', 'QQQ', 'ROKU',
+          'SNAP', 'SPY', 'SQ', 'TSLA', 'TWTR', 'ULTA', 'UPS', 'V', 'VXX',
+          'VDE', 'XLB', 'XLI', 'VCR', 'VDC', 'XLV', 'XLF', 'VGT', 'XLC', 'XLU', 'VNQ']
+
+
+def human_time(epoch):
+    new_time = datetime.fromtimestamp(int(epoch) / 1000)
+    output = new_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    return output
+
+
+def get_time_now():
+    curr_time = time.localtime()
+    curr_clock = time.strftime("%H:%M:%S", curr_time)
+    curr_m = time.strftime('%m')
+    curr_y_d = time.strftime('%d%Y')
+
+    int_curr_clock = int(f'{curr_clock[:2]}{curr_clock[3:5]}')
+
+    return int_curr_clock, curr_m, curr_y_d
+
+
+def get_chain(stock):
+    opt_lookup = TDSession.get_options_chain(
+        option_chain={'symbol': stock, 'strikeCount': 50,
+                      'toDate': '2021-3-26'})
+
+    return opt_lookup
+
+
+def raw_chain(raw, put_call):
+    cp = f'{put_call}ExpDateMap'
+    raw_data = [[]]
+    r = -1
+    for k in raw[cp].keys():
+        # print(k, raw[k], '\n')
+        for strike in raw[cp][k].keys():
+            # print(strike, raw[k][strike])
+            for a in raw[cp][k][strike][0].keys():
+                # if r == -1:
+                #    print(raw[cp][k][strike][0].keys())
+                unit = raw[cp][k][strike][0][a]
+                if unit == put_call.upper():
+                    r = r + 1
+                    if r > 0:
+                        raw_data.append([])
+
+                raw_data[r].append(unit)
+
+    return raw_data
+
+
+def clean_chain(raw):
+    df_cp = pd.DataFrame(raw, columns=opt_column_names)
+    clean = df_cp.drop(columns=columns_unwanted)
+
+    return clean
 
 
 pulls = 0
